6333302f758685fe7678f0e8cce7aaec37ef620b
GThibeault <gabriel.eric.thibeault@gmail.com>
2017-06-28 21:21:13 -0300

    Gramatica alternativa

--- lambda_calculus/parser.py
+++ lambda_calculus/parser.py
@@ -3,19 +3,44 @@ import ply.yacc as yacc
 from .lexer import tokens, LambdaType, LBool, LNat, LLambda
 
 
+########################## EXPRESSIO############################################
+#
+#  E -> S L
+#     | S
+#
+################################################################################
 
-def p_expr_app(p):
-    'expr : func arg'
-    p[0] = p[1].eval_in(p[2])
+def p_expr_s_lambda(p):
+    'expr : S lambda'
 
-def p_expr_lambda(p):
-    'expr : lambda'
+def p_expr_s(p):
+    'expr : S'
+
+########################## S ###################################################
+#
+#  S -> S cont
+#     | λ
+#
+################################################################################
+
+def p_s_factor(p):
+    'S : S cont'
+
+def p_s_empty(p):
+    'S : '
+
+########################## cont ################################################
+#
+#  cont -> (E)
+#     | Var
+#     | Val
+#     | if expr then expr else S
+#
+################################################################################
+
+def p_cont_expr(p):
+    'cont : LPARENS expr RPARENS'
     p[0] = p[1]
-
-def p_expr_cont(p):
-    'expr : cont'
-    p[0] = p[1]
-
 
 def p_cont_true(p):
     'cont : TRUE'
@@ -30,7 +55,7 @@ def p_cont_zero(p):
     p[0] = p[1]
 
 def p_cont_ifthenelse(p):
-    'cont : IF expr THEN expr ELSE expr'
+    'cont : IF expr THEN expr ELSE S'
     print(list(p))
     if p[2].type().is_bool():
         if p[2].value():
@@ -44,45 +69,45 @@ def p_cont_var(p):
     p[0] = p[1]
 
 
-########################## FUNCTION ############################################
+########################### FUNCTION ############################################
+##
+##  F -> V
+##     | (L)
+##     | F A
+##
+#################################################################################
 #
-#  F -> V
-#     | (L)
-#     | F A
+#def p_func_expr(p):
+#    'func : VAR'
+#    p[0] = p[1]
 #
-################################################################################
-
-def p_func_expr(p):
-    'func : VAR'
-    p[0] = p[1]
-
-def p_func_lambda(p):
-    'func : LPARENS lambda RPARENS'
-    p[0] = p[2]
-
-def p_func_app(p):
-    'func : func arg'
-    p[0] = p[1].eval_in(p[2])
-
-########################## ARGUMENT ############################################
+#def p_func_lambda(p):
+#    'func : LPARENS lambda RPARENS'
+#    p[0] = p[2]
 #
-#  A -> C
-#     | (L)
-#     | (F A)
+#def p_func_app(p):
+#    'func : func arg'
+#    p[0] = p[1].eval_in(p[2])
 #
-################################################################################
-
-def p_arg_expr(p):
-    'arg : cont'
-    p[0] = p[1]
-
-def p_arg_lambda(p):
-    'arg : LPARENS lambda RPARENS'
-    p[0] = p[2]
-
-def p_arg_app(p):
-    'arg : LPARENS func arg RPARENS'
-    p[0] = p[2].eval_in(p[3])
+########################### ARGUMENT ############################################
+##
+##  A -> C
+##     | (L)
+##     | (F A)
+##
+#################################################################################
+#
+#def p_arg_expr(p):
+#    'arg : cont'
+#    p[0] = p[1]
+#
+#def p_arg_lambda(p):
+#    'arg : LPARENS lambda RPARENS'
+#    p[0] = p[2]
+#
+#def p_arg_app(p):
+#    'arg : LPARENS func arg RPARENS'
+#    p[0] = p[2].eval_in(p[3])
 
 ########################## LAMBDA ##############################################
 #
